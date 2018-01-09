13d548598398747b1350179a572f0651bfcf9ff4
unknown <paul_schaller@att.net>
2018-01-09 15:16:33 -0500

    sending through first copy of working program

--- liri.js
+++ liri.js
@@ -9,19 +9,19 @@ var variableValue = process.argv[3];
 
 
 
- //tweets
+ if(choice === 'my-tweets'){
+ 	 //tweets
 var client = new Twitter(keys.twitter);
  
 
 var params = {screen_name: 'Paul Schaller', count: 20};
- if(choice === 'my-tweets'){
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     for(var i = 0; i < tweets.length; i++){
     	  var twitterTweets = 
-    	  	tweets[i].user.screen_name +": " + 
-	        tweets[i].text + "\n" + 
-	        tweets[i].created_at + "\n" + 
+    	  	"Screen Name:  " + tweets[i].user.screen_name + "\n" + 
+	        "text:  " + tweets[i].text + "\n" + 
+	        "Date Created:  " + tweets[i].created_at + "\n" + 
 	        "Tweet #" + i + "\n";
 	       console.log(twitterTweets);
 	       log(twitterTweets);
@@ -36,40 +36,38 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
 
 
 
-//spotify 
-var song = new Spotify(keys.spotify);
 
 if(choice === 'spotify-this-song'){ 
-	var songTitle = process.argv[3];
 
-	if(!songTitle){
-			songTitle = 'The Sign';
+	var song = new Spotify(keys.spotify);
+	// Spotify function, uses the Spotify module to call the Spotify api
+		var songTitle = process.argv[3];
+		if(!songTitle){
+			songTitle = "The Sign";
 		}
 	
-	console.log(songTitle); 
-	
-	song.search({ type: 'track', query: songTitle }, function(err, data) {
-  		console.log(data);
-  		console.log(data.album.name);
-  		if (err) {
-    		return console.log('Error: ' + err);
-  			} else {
-						var songInfo = data.tracks.items;
-						console.log(songInfo.artists[0].name);
+		song.search({ type: "track", query: songTitle }, function(err, data) {
+			if(!err){
+				var songInfo = data.tracks.items;
+				for (var i = 0; i < 5; i++) {
+					if (songInfo[i] != undefined) {
 						var spotifySongs =
-							"Artist: " + songInfo.artists[0].name + "\n" +
-							"Song: " + songInfo.name + "\n" +
-							"Preview Link: " + songInfo.preview_url+ "\n" + 
-							"Album the song is from: " + songInfo.album.name + "\n";
-						
+						"Artist: " + songInfo[i].artists[0].name + "\r\n" +
+						"Song: " + songInfo[i].name + "\r\n" +
+						"Preview Url: " + songInfo[i].preview_url + "\r\n" + 
+						"Album the song is from: " + songInfo[i].album.name + "\r\n" +
+						"Song #" + i + "\r\n";
 						console.log(spotifySongs);
 						log(spotifySongs); // calling log function
-					
-				
+					}
+				}
+			}	else {
+				console.log("Error :"+ err);
+				return;
 			}
 		});
 
-}
+	}	
 
 
 if(choice === 'movie-this'){
@@ -78,15 +76,10 @@ if(choice === 'movie-this'){
 		if(!movie){
 			movie = "mr nobody";
 		}
-		console.log(movie);
-		request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
+		request("http://www.omdbapi.com/?apikey=trilogy&t=" + movie + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
 			if (!error && response.statusCode == 200) {
-				console.log(body);
 				var movieItem = JSON.parse(body);
-				 console.log(movieItem);
-				//console.log(movieItem); // Show the text in the terminal
 				var movieListings =
-				"------------------------------ begin ------------------------------" + "\r\n"
 				"Title: " + movieItem.Title+"\r\n"+
 				"Year: " + movieItem.Year+"\r\n"+
 				"Imdb Rating: " + movieItem.imdbRating+"\r\n"+
@@ -94,11 +87,9 @@ if(choice === 'movie-this'){
 				"Country: " + movieItem.Country+"\r\n"+
 				"Language: " + movieItem.Language+"\r\n"+
 				"Plot: " + movie.Plot+"\r\n"+
-				"Actors: " + movieItem.Actors+"\r\n"+
-				"------------------------------ fin ------------------------------" + "\r\n";
-				"------------------------------ fin ------------------------------" + "\r\n";
-				console.log(Listings);
-				log(movieListing); // calling log function
+				"Actors: " + movieItem.Actors+"\r\n";
+				console.log(movieListings);
+				log(movieListings); // calling log function
 			} else {
 				console.log("Error :"+ error);
 				return;
@@ -108,16 +99,19 @@ if(choice === 'movie-this'){
 
 if (choice === 'do-what-it-says') {// Do What It Says function, uses the reads and writes module to access the random.txt file and do what's written in it
 		fs.readFile("random.txt", "utf8", function(error, data){
-			console.log(data);
 			if (!error) {
-				//var dataArr = data.split(",");
-				spotifyThisSongA(data);
+				var dataArr = data.split(",");
+				spotifyThisSongA(dataArr[1]);
 			} else {
 				console.log("Error occurred" + error);
 			}
 		});
 
 }
+
+
+
+
 	// Do What It Says function, uses the reads and writes module to access 
 	//the log.txt file and write everything that returns in terminal in the log.txt file
 	function log(logResults) {
@@ -129,6 +123,30 @@ if (choice === 'do-what-it-says') {// Do What It Says function, uses the reads a
 	}
 
 
-function spotifyThisSongA(data){
-	console.log(data);
-}
+	function spotifyThisSongA(songTitle){
+
+
+var song = new Spotify(keys.spotify);
+	// Spotify function, uses the Spotify module to call the Spotify api
+
+		song.search({ type: "track", query: songTitle }, function(err, data) {
+			if(!err){
+				var songInfo = data.tracks.items;
+				for (var i = 0; i < 5; i++) {
+					if (songInfo[i] != undefined) {
+						var spotifySongs =
+						"Artist: " + songInfo[i].artists[0].name + "\r\n" +
+						"Song: " + songInfo[i].name + "\r\n" +
+						"Preview Url: " + songInfo[i].preview_url + "\r\n" + 
+						"Album the song is from: " + songInfo[i].album.name + "\r\n" +
+						"Song #" + i + "\r\n";
+						console.log(spotifySongs);
+						log(spotifySongs); // calling log function
+					}
+				}
+			}	else {
+				console.log("Error :"+ err);
+				return;
+			}
+		});
+	}
